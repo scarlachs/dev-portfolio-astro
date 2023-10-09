5fcff100e514ba163be1cc08190c62a2c7b76ecf
Pascal Schaar <pascal.schaar@me.com>
2023-10-09 18:32:58 +0200

    update layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,24 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
+		fontFamily: {
+			sans: ["Montserrat Variable", ...defaultTheme.fontFamily.sans],
+		},
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: "1rem",
+				sm: "2rem",
+				lg: "4rem",
+				xl: "5rem",
+				"2xl": "6rem",
+			},
+		},
 		extend: {},
 	},
+	darkMode: "class",
 	plugins: [],
-}
+};
