80dd49972554e38f8214f51a8058ef8abd66a1d1
Pascal <butler.ergot_0s@icloud.com>
2024-03-14 23:53:58 +0100

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,4 +11,6 @@ export default defineConfig({
 			applyBaseStyles: false,
 		}),
 	],
+	output: "server",
+	adapter: vercel(),
 });
