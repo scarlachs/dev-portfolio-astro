299fc0b5347e7573e39d2d89dd865050423bfa70
Pascal <butler.ergot_0s@icloud.com>
2024-03-15 23:38:19 +0100

    fix whitespace add focus design for links

--- src/components/MetaNavigation.tsx
+++ src/components/MetaNavigation.tsx
@@ -4,13 +4,13 @@ export const MetaNavigation = () => {
 	return (
 		<nav
 			aria-label="Meta Navigation"
-			className="md:border-l md:border-slate-400 md:pl-5 md:dark:border-slate-600"
+			className="md:border-l md:border-slate-400 md:pl-4 md:dark:border-slate-600"
 		>
-			<ul className="flex flex-col items-center gap-2 md:flex-row md:gap-5">
+			<ul className="flex flex-col items-center gap-2 md:flex-row md:gap-1">
 				{metaNavigationData.map((item) => (
 					<li key={item}>
 						<a
-							className="flex py-2 text-sm text-slate-600 transition-colors hover:text-slate-800 dark:text-slate-400 dark:hover:text-slate-200 max-md:w-full"
+							className="flex rounded-md p-2 text-sm text-slate-600 transition-colors hover:text-slate-800 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 dark:text-slate-400 dark:hover:text-slate-200 dark:focus-visible:ring-slate-300 max-md:w-full"
 							href={`/${item.toLowerCase()}`}
 						>
 							{item}
