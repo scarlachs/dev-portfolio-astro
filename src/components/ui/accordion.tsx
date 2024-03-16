0decc5fb3d039dadcbd7418ae50d11be61cceb7a
Pascal <butler.ergot_0s@icloud.com>
2024-03-16 17:32:28 +0100

    style: fix accordion text-align

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -29,7 +29,7 @@ const AccordionTrigger = React.forwardRef<
 		<AccordionPrimitive.Trigger
 			ref={ref}
 			className={cn(
-				"flex flex-1 items-center justify-between py-4 text-lg font-bold hover:underline motion-safe:transition-all [&[data-state=open]>svg]:rotate-180",
+				"flex flex-1 items-center justify-between py-4 text-start text-lg font-bold hover:underline motion-safe:transition-all [&[data-state=open]>svg]:rotate-180",
 				className,
 			)}
 			{...props}
