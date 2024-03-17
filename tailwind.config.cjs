44b118b0426e17779708ea2a9350ff5e506cc65d
Pascal <butler.ergot_0s@icloud.com>
2024-03-17 18:05:51 +0100

    update config to v3.4.1

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	darkMode: ["class"],
+	darkMode: ["selector"],
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	prefix: "",
 	theme: {
