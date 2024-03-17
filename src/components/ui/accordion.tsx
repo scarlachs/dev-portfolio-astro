57491cca95bb470a6c33117f372dcc0e9e59a7a6
Pascal <butler.ergot_0s@icloud.com>
2024-03-17 18:06:32 +0100

    style: add focus outline

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -29,7 +29,7 @@ const AccordionTrigger = React.forwardRef<
 		<AccordionPrimitive.Trigger
 			ref={ref}
 			className={cn(
-				"flex flex-1 items-center justify-between py-4 text-start text-lg font-bold hover:underline motion-safe:transition-all [&[data-state=open]>svg]:rotate-180",
+				"flex flex-1 items-center justify-between rounded-md py-4 text-start text-lg font-bold hover:underline focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 motion-safe:transition-all dark:focus-visible:ring-slate-300 [&[data-state=open]>svg]:rotate-180",
 				className,
 			)}
 			{...props}
